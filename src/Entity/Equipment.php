d68c0568ab43ab8cb910e3d0921d66a5931c270b
Michel Bondonga <bondongamichel@gmail.com>
2024-01-15 13:40:30 +0100

    Création de l'entité Equipment

---
+++ src/Entity/Equipment.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipmentRepository::class)]
+class Equipment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'equipments')]
+    private ?Room $rooms = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRooms(): ?Room
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(?Room $rooms): static
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+}
