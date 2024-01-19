23566cdf4695b08d3467df9caff923d45bc07e33
Michel Bondonga <bondongamichel@gmail.com>
2024-01-19 12:01:09 +0100

    style(app.css):remove unnecessary empty line

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,11 +8,51 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'paris')]
+    #[Route('/', name: 'paris', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
-            
+        return $this->render('page/city.html.twig', [
+            'title' => 'Paris',
+            'background' => 'paris',
+        ]);
+    }
+
+    #[Route('/lasvegas', name: 'lasvegas', methods: ['GET'])]
+    public function lasvegas(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Las Vegas',
+            'background' => 'lasvegas',
+        ]);
+    }
+
+
+    #[Route('/kyoto', name: 'kyoto', methods: ['GET'])]
+    public function kyoto(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Kyoto',
+            'subtitle'=> '京都市',
+            'background' => 'kyoto',
+        ]);
+    }
+
+    #[Route('/sydney', name: 'sydney', methods: ['GET'])]
+    public function sydney(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Sydney',
+            'background' => 'sydney',
+        ]);
+    }
+
+    #[Route('/hongkong', name: 'hongkong', methods: ['GET'])]
+    public function hongkong(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Hong Kong',
+            'subtitle' => '香港',
+            'background' => 'hongkong',
         ]);
     }
 
