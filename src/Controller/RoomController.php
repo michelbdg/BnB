a918dd2678f119cd61acb26046a94250334da0cc
Michel Bondonga <bondongamichel@gmail.com>
2024-01-22 09:19:38 +0100

    Ajout(roomController.php): ajouter un contrôleur pour gérer mes salles

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -7,15 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/r')] //prefix all room routes with /r
 class RoomController extends AbstractController
 {
-    #[Route('/room', name: 'app_room')]
+    #[Route('/', name: 'app_room')]
     public function index(
         RoomRepository $roomRepository
     ): Response
     {
         return $this->render('room/index.html.twig', [
-            'room' => $roomRepository->findAll(),
+            'rooms' => $roomRepository->findAll(),
         ]);
     }
 }
