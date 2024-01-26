9dad3390cd4f9c18ef76207c5c85ba7714589629
Michel Bondonga <bondongamichel@gmail.com>
2024-01-26 09:37:32 +0100

    - Ajouter(UserCrudController.php) : ajouter le contrôleur UserCrudController pour gérer les opérations CRUD sur l'entité User

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the labels used to refer to this entity in titles, buttons, etc.
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
+
+            // in addition to a string, the argument of the singular and plural label methods
+            // can be a closure that defines two nullable arguments: entityInstance (which will
+            // be null in 'index' and 'new' pages) and the current page name
+            ->setEntityLabelInSingular(
+                fn (?User $user, ?string $pageName) => $user ? $user->__toString() : 'Utilisateur'
+            )
+            ;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
