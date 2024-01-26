75742901b63ca7de5b21a0eff8d570b09685234e
Michel Bondonga <bondongamichel@gmail.com>
2024-01-26 16:55:35 +0100

    - Ajout de la méthode toString()

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -314,4 +314,9 @@ class Room
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->title;
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -454,4 +454,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->getFullname();
+    }
 }
