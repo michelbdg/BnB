b9d4729e6063a065519c2239c45f682674496e8f
Michel Bondonga <bondongamichel@gmail.com>
2024-01-26 09:38:11 +0100

    - Ajouter(DashboardController.php) : ajouter un contrôleur pour le tableau de bord de l'administrateur.

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Room;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        // return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('user/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('<img src="/images/icon.svg" width="50">')
+            ->setFaviconPath('/images/icon.svg')
+            ->renderContentMaximized()
+            ->setLocales([
+                'en' => '🇬🇧 English',
+                'pl' => '🇵🇱 Polski'
+            ]);
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Assets');
+        yield MenuItem::linkToCrud('Locations', 'fas fa-cubes', Room::class);
+        yield MenuItem::section('Utilisateur');
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-users', User::class);
+        yield MenuItem::linkToRoute('Back to app', 'fas fa-arrow-left', 'app_room');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getFullname())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            ->setAvatarUrl('/uploads/users/'. $user->getImage())
+            
+            ->addMenuItems([
+                MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            ]);
+    }
+}
